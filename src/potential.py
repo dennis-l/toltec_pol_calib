d0ef53ce95c46b7b0aef7a843f2d74488f9e613e
Dennis Lee <dennislee2023@u.northwestern.edu>
2022-04-13 12:33:20 -0500

    update amapola plots

--- src/potential.py
+++ src/potential.py
@@ -69,5 +69,5 @@ if __name__ == "__main__":
         sp_obj[4].legend(fancybox=False, loc='lower left', handletextpad=0.7, frameon=False)
 
         plot_obj.tight_layout()
-        plot_obj.savefig(f'./plots/potential/{name_3c}.png', dpi=250)
+        plot_obj.savefig(f'./plots/amapola/{name_3c}.png', dpi=250)
         plt.close('all')
